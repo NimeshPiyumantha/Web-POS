36160052c329d0f9aad0b10787b5477b3aa2e307
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-17 23:55:35 +0530

    Add Customer Model

---
+++ model/customer.js
@@ -0,0 +1,16 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+/**
+ * Class declarations
+ */
+class customer{
+    constructor(id,name,address,salary) {
+        this.id=id;
+        this.name=name;
+        this.address=address;
+        this.salary=salary;
+    }
+}
