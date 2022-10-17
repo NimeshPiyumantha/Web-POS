712fb6baeb0fb9437384f8ef69b9d600ebb6a411
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-17 23:43:54 +0530

    Add Home Section

---
+++ controllers/DashboardController.js
@@ -0,0 +1,25 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+$(document).ready(function () {
+    $("#HomeSection").css('display', 'block');
+    $("#CustomerSection").css('display', 'none');
+    $("#ItemSection").css('display', 'none');
+    $("#OrderSection").css('display', 'none');
+    $("#OrderDetails").css('display', 'none');
+});
+
+$("#homeBtn").click(function () {
+    $("#HomeSection").css('display', 'block');
+    $("#CustomerSection").css('display', 'none');
+    $("#ItemSection").css('display', 'none');
+    $("#OrderSection").css('display', 'none');
+    $("#OrderDetails").css('display', 'none');
+});
+
+
+
+
+
